bddea4ce67f4d1c5ea5b1c217ff7e5e53ffc1c5f
boomNDS <boomsixboom@gmail.com>
2018-12-18 13:36:35 +0700

    flag animation

--- core/assets/Tile_map.tsx
+++ core/assets/Tile_map.tsx
@@ -1,6 +1,24 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.2.0" name="Tile_map" tilewidth="32" tileheight="32" tilecount="1056" columns="44">
  <image source="../Abstract Platformer (370 assets)/Tilesheet/tilesheet_complete.png" width="1408" height="768"/>
+ <tile id="732">
+  <animation>
+   <frame tileid="732" duration="500"/>
+   <frame tileid="734" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="733">
+  <animation>
+   <frame tileid="733" duration="500"/>
+   <frame tileid="735" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="776">
+  <animation>
+   <frame tileid="776" duration="500"/>
+   <frame tileid="778" duration="500"/>
+  </animation>
+ </tile>
  <tile id="904">
   <animation>
    <frame tileid="904" duration="500"/>
